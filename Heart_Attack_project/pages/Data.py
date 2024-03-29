d3fa45b83d8d4021de956d42523cc31d5611a399
mahmoudabdalla26 <112134663+mahmoudabdalla26@users.noreply.github.com>
2024-03-29 15:30:21 +0200

    Update Data.py

--- Heart_Attack_project/pages/Data.py
+++ Heart_Attack_project/pages/Data.py
@@ -23,5 +23,5 @@ st.write('Heart Disease Dataset After Cleaning And Preprocessing:')
 st.markdown('<a href="https://drive.google.com/file/d/1-v58el0--iKMBrYizO8XEi13VJYyc0mA/view?usp=sharing"> <center> Link to Dataset  </center> </a> ', unsafe_allow_html=True)
 
 # Reading and displaying the cleaned dataset
-df = pd.read_csv(r"C:\Users\Lenovo\Desktop\mid\Heart_Attack_project\Sourse\cleaned_data.csv")
+df = pd.read_csv("./Heart_Attack_project/Sourse/cleaned_data.csv")
 st.write(df)
