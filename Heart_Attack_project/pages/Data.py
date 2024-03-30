b08bbea73fcde0c3c44e778702d7243f57f44fbe
mahmoudabdalla26 <112134663+mahmoudabdalla26@users.noreply.github.com>
2024-03-31 00:46:00 +0200

    Update Data.py

--- Heart_Attack_project/pages/Data.py
+++ Heart_Attack_project/pages/Data.py
@@ -1,14 +1,7 @@
 # Import necessary libraries
 import pandas as pd 
 import streamlit as st 
-
 from zipfile import ZipFile
-zip_file_path = 'Heart_Attack_project/Sourse/heart_2022_with_nans.zip'
-with ZipFile(zip_file_path, 'r') as zip_ref:
-    zip_ref.extractall()
-extracted_files = zip_ref.namelist()
-csv_file_name = extracted_files[0] 
-df = pd.read_csv(csv_file_name)
 
 # Title for the page
 st.markdown(" <center>  <h1> Heart Disease Dataset </h1> </font> </center> </h1> ",
@@ -21,6 +14,12 @@ st.write('Heart Disease Dataset Before Cleaning And Preprocessing:')
 st.markdown('<a href="https://www.kaggle.com/datasets/kamilpytlak/personal-key-indicators-of-heart-disease"> <center> Link to Dataset  </center> </a> ', unsafe_allow_html=True)
 
 # Reading and displaying the dataset before cleaning and preprocessing
+zip_file_path = 'Heart_Attack_project/Sourse/heart_2022_with_nans.zip'
+with ZipFile(zip_file_path, 'r') as zip_ref:
+    zip_ref.extractall()
+extracted_files = zip_ref.namelist()
+csv_file_name = extracted_files[0] 
+df = pd.read_csv(csv_file_name)
 #df = pd.read_csv("./Heart_Attack_project/Sourse/heart_2022_with_nans.zip", compression='gzip')
 st.write(df)
 
@@ -31,5 +30,11 @@ st.write('Heart Disease Dataset After Cleaning And Preprocessing:')
 st.markdown('<a href="https://drive.google.com/file/d/1-v58el0--iKMBrYizO8XEi13VJYyc0mA/view?usp=sharing"> <center> Link to Dataset  </center> </a> ', unsafe_allow_html=True)
 
 # Reading and displaying the cleaned dataset
-df = pd.read_csv("./Heart_Attack_project/Sourse/cleaned_data.zip", compression='gzip')
+zip_file_path = 'Heart_Attack_project/Sourse/cleaned_data.zip'
+with ZipFile(zip_file_path, 'r') as zip_ref:
+    zip_ref.extractall()
+extracted_files = zip_ref.namelist()
+csv_file_name = extracted_files[0] 
+df = pd.read_csv(csv_file_name)
+#df = pd.read_csv("./Heart_Attack_project/Sourse/cleaned_data.zip", compression='gzip')
 st.write(df)
