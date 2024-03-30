250a696d924795e6619bb8208e0399656934d042
mahmoudabdalla26 <112134663+mahmoudabdalla26@users.noreply.github.com>
2024-03-31 00:33:38 +0200

    Update Data.py

--- Heart_Attack_project/pages/Data.py
+++ Heart_Attack_project/pages/Data.py
@@ -2,6 +2,15 @@
 import pandas as pd 
 import streamlit as st 
 
+
+from zipfile import ZipFile
+zip_file_path = 'heart_2022_with_nans.zip'
+with ZipFile(zip_file_path, 'r') as zip_ref:
+    zip_ref.extractall()
+extracted_files = zip_ref.namelist()
+csv_file_name = extracted_files[0] 
+df = pd.read_csv(csv_file_name)
+
 # Title for the page
 st.markdown(" <center>  <h1> Heart Disease Dataset </h1> </font> </center> </h1> ",
             unsafe_allow_html=True)
@@ -13,7 +22,7 @@ st.write('Heart Disease Dataset Before Cleaning And Preprocessing:')
 st.markdown('<a href="https://www.kaggle.com/datasets/kamilpytlak/personal-key-indicators-of-heart-disease"> <center> Link to Dataset  </center> </a> ', unsafe_allow_html=True)
 
 # Reading and displaying the dataset before cleaning and preprocessing
-df = pd.read_csv("./Heart_Attack_project/Sourse/heart_2022_with_nans.zip", compression='gzip')
+#df = pd.read_csv("./Heart_Attack_project/Sourse/heart_2022_with_nans.zip", compression='gzip')
 st.write(df)
 
 # Displaying information about the dataset after cleaning and preprocessing
